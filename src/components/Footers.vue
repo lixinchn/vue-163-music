78f03c35e8801b8c4968638a499a1efd672b87fe
lixin <lixinit@gmail.com>
2017-03-23 16:49:28 +0800

    play progress

--- src/components/Footers.vue
+++ src/components/Footers.vue
@@ -6,7 +6,9 @@
       <img class="next" src="../assets/music_next.png">
     </div>
     <div class="progress">
-      <vue-slider v-model="value"></vue-slider>
+      <span>0:00</span>
+      <vue-slider ref="slider" v-bind="setting" style="display: inline-block; padding: 15px 10px;" v-model="value"></vue-slider>
+      <span>4:50</span>
     </div>
   </div>
 </template>
@@ -23,6 +25,14 @@ export default {
     return {
       app: '网易云音乐',
       value: 1,
+      setting: {
+        width: 500,
+        tooltip: false,
+        processStyle: {
+          'background-color': 'rgb(232, 60, 60)'
+        },
+        sliderStyle: null,
+      },
     }
   }
 }
@@ -64,5 +74,24 @@ export default {
 .progress {
   display: inline-block;
   float: left;
+  height: 100%;
+  line-height: 50px;
+  margin-left: 20px;
+}
+
+.progress span{
+  position: relative;
+  top: -10px;
+}
+</style>
+
+<style>
+.star-slider, .vue-slider, .vue-slider-dot {
+  background: url('../assets/point.png') no-repeat;
+  background-size: 10px 10px;
+  box-shadow: none;
+  border-radius: 0;
+  position: relative;
+  background-position: 3px;
 }
 </style>
