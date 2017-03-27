b8b9bfd2d9326ddc9d68685a3f8385987d61bd33
lixin <lixinit@gmail.com>
2017-03-28 00:04:42 +0800

    router

---
+++ src/components/find/index.js
@@ -0,0 +1,18 @@
+import Recommend from './recommend'
+import Song from './song'
+import Nav from './nav'
+import Station from './station'
+import Rank from './rank'
+import New from './new'
+import Singer from './singer'
+
+
+export default {
+  Recommend,
+  Song,
+  Nav,
+  Station,
+  Rank,
+  New,
+  Singer,
+}
